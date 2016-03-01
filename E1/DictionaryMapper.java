a66e04b06775a26f70bb7c96be853ef9afbabd52
Manoranjan Sriramagiri <manoranjan.sriramagiri@gmail.com>
2016-03-01 02:01:09 -0800

    Added basic functionality.

--- E1/DictionaryMapper.java
+++ E1/DictionaryMapper.java
@@ -5,17 +5,24 @@ import java.util.StringTokenizer;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.conf.Configuration;
 
-public class DictionaryMapper  extends Mapper<Text, Text, Text, Text> {
-      // TODO define class variables for translation, language, and file name
-	//dklnldsnln
-      public void setup(Context context) {    	   
-      // TODO determine the language of the current file by looking at it's name
-      }
+public class DictionaryMapper extends Mapper<Text, Text, Text, Text> {
+	// TODO define class variables for translation, language, and file name
+	// dklnldsnln
+	/*
+	 * public void setup(Context context) { Configuration conf =
+	 * context.getConfiguration(); }
+	 */
+	private Text word = new Text();
 
-      public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
-      // TODO instantiate a tokenizer based on a regex for the file
+	public void map(Text key, Text value, Context context) throws IOException,
+			InterruptedException {
 
-      // iterate through the tokens of the line, parse, transform and write to context
-      }
+		StringTokenizer itr = new StringTokenizer(value.toString(), ",");
+		while (itr.hasMoreTokens()) {
+			word.set(itr.nextToken());
+			context.write(key, word);
+		}
+	}
 }
